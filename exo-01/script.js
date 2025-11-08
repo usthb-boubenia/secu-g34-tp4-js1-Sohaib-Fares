80030c3da1a811fb652735fd617980eb18cc31e6
svrxo <info.sohaib.fares@gmail.com>
2025-11-08 17:34:41 +0100

    feat: add table and display values when buttons are clicked.

---
+++ exo-01/script.js
@@ -0,0 +1,43 @@
+"use strict";
+
+//Exo 01
+//Let: slide 50
+//const: slide 51/37
+
+//Function 01: Display the 'nom' "John" in the console
+function f1(){
+    let nom = "John";
+    console.log(nom);
+}
+
+//Function 02: Display the 'prénom' "Doe" in the console
+function f2(){
+    let prenom = "Doe";
+    console.log(prenom);
+}
+
+//Function 03: Display the 'nom & prénom' in the console concatenated
+function f3(){
+    let nom = "John";
+    let prenom = "Doe";
+    console.log(nom + "-" + prenom); //String concatination using '+' operator.
+}
+
+//Fonction 04: Display the 'âge'
+function f4() {
+    let age = 30;
+    console.log(age);
+}
+
+//Function 05: Display the 'note'
+function f5(){
+    let note = 15;
+    console.log(note);
+}
+
+//Function 06: Display 'Pi'
+function f6(){
+    const pi = 3.14;
+    console.log(pi);
+}
+
