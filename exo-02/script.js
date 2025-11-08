36e574da4a6baf663b49bb25544eb2dc00601ba2
svrxo <info.sohaib.fares@gmail.com>
2025-11-08 18:17:46 +0100

    Done

---
+++ exo-02/script.js
@@ -0,0 +1,42 @@
+//Exo: 02
+
+//const: slide 51/37
+//console.log() slide 33
+//functions: slide 45
+
+
+function f1(){
+
+    //Array of strings, slide: 38
+    const strudents = ["Etudiant1", "Etudiant2", "Etudiant3"];
+
+    //for..of loop, slide: 44
+    for (const strudent of strudents ){
+        console.log(strudent);
+    }
+}
+
+function f2(){
+    
+    //Object, slide: 39
+    const strudent = {
+        nom: "John",
+        prenom: "Doe",
+        age: 30
+    };
+    console.log(strudent);
+}
+
+function f3(){
+    //Table of objects, slide: 40
+    const strudents = [
+        { nom: "name1", prenom: "surname1", age: 21 },
+        { nom: "name2", prenom: "surname2", age: 22 },
+        { nom: "name3", prenom: "surname3", age: 23 }
+    ];
+
+    //for..of loop, slide: 44
+    for (const strudent of strudents){
+        console.log(strudent);
+    }
+}
